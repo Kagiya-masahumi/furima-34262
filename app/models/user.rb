a4cb958d3c489804669099c64f8d0606bb65e392
Kagiya-masahumi <Kagiya-masahumi@users.noreply.github.com>
2021-04-03 20:55:00 +0900

    ユーザー認証のバリデーションを実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
-        
+  
+    
+  validates :nickname, :name,:family_name,:birthday,presence: true 
+  validates :email,presence: true, uniqueness: true
+  validates_format_of :name_kana,:family_name_kana, presence: true, with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]{6,100}+\z/.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'パスワードには英字と数字の両方を含めて設定してください'
 
-    validates :nickname, :email, :password, :name, :name_kana,:family_name,:family_name_kana,:birthday,presence: true 
+    has_many :items, dependent: :destroy
+    has_many :orders
 end
