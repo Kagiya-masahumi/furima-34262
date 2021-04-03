43e39a405d5698888dd414d2fc82193c3d531da3
Kagiya-masahumi <Kagiya-masahumi@users.noreply.github.com>
2021-04-03 16:50:07 +0900

    deviseの導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
+        
 
-  has_many :items, dependent: :destroy
-  has_many :orders
-
-  validates :nickname,:email,:password,:name,:name_kana,:family_name,:family_name_kana,:birthday,presence: true
-
+    validates :nickname, :email, :password, :name, :name_kana,:family_name,:family_name_kana,:birthday,presence: true 
 end
