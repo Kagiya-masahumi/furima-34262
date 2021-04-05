7832332b7562716e4a405fd45e570d0c5cb74ea3
Kagiya-masahumi <Kagiya-masahumi@users.noreply.github.com>
2021-04-05 19:22:30 +0900

    new・createコントローラーを実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,5 +6,14 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:items).permit(:name, :status_id, :category_id, :item_explain, :carry_date_id, :carry_area_id, :postage_id, :price, :image).merge(user_id: current_user.id)
+  end
   
 end
