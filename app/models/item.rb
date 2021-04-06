4cb7961f043684b3b99a6c5b20880c863a2c9390
Kagiya-masahumi <Kagiya-masahumi@users.noreply.github.com>
2021-04-06 18:33:51 +0900

    ActiveHashを用いた各テーブルのアソシエーションを実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,8 @@
 class Item < ApplicationRecord
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :status, :category, :carry_date, :carry_area, :postage
+
   has_one :order
   belongs_to :user 
   has_one_attached :image
