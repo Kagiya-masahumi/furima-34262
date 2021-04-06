84cf514db4ddbc4e931ccd09af4892a15c788297
Kagiya-masahumi <Kagiya-masahumi@users.noreply.github.com>
2021-04-06 18:42:07 +0900

    itemsテーブルのバリデーションを実装

--- db/migrate/20210405063643_create_items.rb
+++ db/migrate/20210405063643_create_items.rb
@@ -4,7 +4,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.string :name           ,null: false
       t.integer :status_id     ,null: false
       t.integer :category_id   ,null: false
-      t.text :item_explain     ,null: false
+      t.text :explain          ,null: false
       t.integer :carry_date_id ,null: false
       t.integer :carry_area_id ,null: false
       t.integer :postage_id    ,null: false
