f328ea180370027ea1e97c4d90572d68f3cbb593
Kagiya-masahumi <Kagiya-masahumi@users.noreply.github.com>
2021-04-07 19:41:32 +0900

    エラーメッセージを日本語化

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,6 +17,7 @@ class ItemsController < ApplicationController
     end
   end
 
+
   private
   def item_params
     params.require(:item).permit(:name, :status_id, :category_id, :explain, :carry_date_id, :carry_area_id, :postage_id, :price, :image).merge(user_id: current_user.id)
