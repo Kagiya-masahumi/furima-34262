4ea1b6ad2013a4d384a5e95434e338cdd9d329c8
Kagiya-masahumi <Kagiya-masahumi@users.noreply.github.com>
2021-04-07 21:12:28 +0900

    販売価格のバリデーションを実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
   belongs_to :user 
   has_one_attached :image
 
-  validates :name, :explain, presence: true
+  validates :name, :explain, :price, presence: true
   validates :status_id, :category_id, :carry_area_id, :carry_date_id, :postage_id, numericality: { other_than: 1 }
 
 end
