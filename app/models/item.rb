b8009b8357cb1b5c9affb33303bb8969301950a4
Kagiya-masahumi <Kagiya-masahumi@users.noreply.github.com>
2021-04-10 18:38:56 +0900

    エラーメッセージを訂正

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,7 @@ class Item < ApplicationRecord
   belongs_to :carry_area, :carry_date, :postage, :status, :category
 
   validates :name, :explain,:image, presence: true
-  validates :status_id, :category_id, :carry_area_id, :carry_date_id, :postage_id, numericality: { other_than: 1 ,message: 'を選択してください'}
+  validates :status_id, :category_id, :carry_area_id, :carry_date_id, :postage_id, numericality: { other_than: 1 }
   validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
 
 end
