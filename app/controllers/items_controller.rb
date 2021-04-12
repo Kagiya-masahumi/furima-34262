83776d7c8928a4c96d9b3f1679330d2e2fd212df
Kagiya-masahumi <Kagiya-masahumi@users.noreply.github.com>
2021-04-12 18:24:51 +0900

    ユーザーが、自身が出品していない状態でURLに直接入力すると、トップページへ遷移する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,7 +25,7 @@ class ItemsController < ApplicationController
   def edit
     @item = Item.find(params[:id])
     if @item.user != current_user
-      redirect_to item_path
+      redirect_to root_path
     end
   end
 
