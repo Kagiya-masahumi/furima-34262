a29131f522c0de19ad8259aa35673f6487e1ea30
Kagiya-masahumi <Kagiya-masahumi@users.noreply.github.com>
2021-04-16 17:48:02 +0900

    フォームオブジェクトのエラーメッセージを表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,16 @@
 class OrdersController < ApplicationController
-  before_action :block_out_page, only:[:index, :create]
+  before_action :authenticate_user!
 
   def index
+
     @item = Item.find(params[:item_id])
+
     @order_address = OrderAddress.new
   end
 
 
   def create
+    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
       @order_address.save
@@ -21,14 +24,9 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_address).permit(:postal_num, :prefecture, :city, :address, :building, :phone_num)
-                                  .merge(user_id: current_user, item_id: current_user.item)
+                                  .merge(user_id: current_user, item_id: params[:item_id])
   end
 
-  def block_out_page
-    if @item.user != current_user
-      redirect_to root_path
-    end
-  end
 
 
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,26 +1,20 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :item_id, :user_id, :postal_num, :prefecture_id, :city, :address, :building, :phone_num
+  attr_accessor :item_id, :user_id, :postal_num, :prefecture, :city, :address, :building, :phone_num
 
   with_options presence: true do
     validates :user_id
     validates :item_id 
     validates :postal_num,format:{with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture
-    validates :city
+    validates :prefecture,numericality: {other_than: 0, message: "can't be blank"}
     validates :address
     validates :phone_num, format:{with: /\A[0-9]{11}\z/}
   end
 
   def save
     order = Order.create(item_id: item_id, user_id: user_id)
-    Address.create(postal_code: postal_code,
-                    prefecture: prefecture, 
-                    city: city, 
-                    address: address, 
-                    building: building,
-                    phone_num: phone_num,
-                    order_id: order.id)
+    Address.create(postal_num: postal_num, prefecture: prefecture,  city: city,  address: address, 
+                    building: building, phone_num: phone_num, order_id: order.id)
   end
 
 
